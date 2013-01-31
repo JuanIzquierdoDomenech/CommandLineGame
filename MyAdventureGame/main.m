baf8b508125b308f3ad6d2e27380f8e5fe9bab6d
Juan Jesus Izquierdo Domenech <juanjesusizquierdodomenech@gmail.com>
2013-01-31 17:35:07 +0100

    Initial Commit

---
+++ MyAdventureGame/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  MyAdventureGame
+//
+//  Created by Juan Jesús Izquierdo Doménech on 31/01/13.
+//  Copyright (c) 2013 Juan Jesús Izquierdo Doménech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
